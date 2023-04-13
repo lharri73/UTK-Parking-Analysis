820a04c0ad1a468135759da50ddfe18eb07f6b76
Landon Harris <lharri73@vols.utk.edu>
2023-04-13 13:39:48 -0400

    fill out sizes.json

--- scripts/update_sizes.py
+++ scripts/update_sizes.py
@@ -1,5 +1,7 @@
 import os
 import json
+import csv
+import copy
 
 from utparking.lib.map_parser import Parser
 
@@ -11,6 +13,24 @@ def main():
         'parking': {b.name: 0 for b in p.parking},
         'buildings': {b.name: 0 for b in p.buildings}
     }
+
+    read_buildings = {}
+    with open(os.path.join(data_file, 'BuildingList.csv'), 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            read_buildings[row['BUILDING NAME']] = float(row['NET RM SQFT'])
+
+    for b in p.buildings:
+        try:
+            ret['buildings'][b.name] = read_buildings[b.name]
+        except KeyError:
+            try:
+                ret['buildings'][b.name] = read_buildings[b.name + " Building"]
+            except KeyError:
+                pass
+            print(f"Couldn't find {b.name} in BuildingList.csv")
+            continue
+
     with open(os.path.join(data_file, 'sizes.json'), 'w') as f:
         json.dump(ret, f, indent=4)
 
