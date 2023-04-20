72a65e7072ef1c91d53c4d529cdcc79bc0a8abd0
Landon Harris <lharri73@vols.utk.edu>
2023-04-20 13:42:27 -0400

    fixed sizes

--- scripts/read_new_size.py
+++ scripts/read_new_size.py
@@ -4,7 +4,7 @@ from csv import DictReader
 
 from utparking.lib.map_parser import Parser
 
-DATA_DIR = os.path.join(os.path.basename(__file__), "..", "data")
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 
 
 def find_matching_building(read_buildings, building_name):
@@ -23,7 +23,7 @@ def main():
     with open(os.path.join(DATA_DIR, "bsizes.json"), "r") as f:
         bsizes = json.load(f)
     building_code_map = {}
-    with open(os.path.join(DATA_DIR), "BuildingList.csv") as f:
+    with open(os.path.join(DATA_DIR, "BuildingList.csv"), "r") as f:
         old_sizes = DictReader(f)
         for row in old_sizes:
             building_code_map.update({row["BUILDING NAME"]: row["REG CODE"]})
@@ -35,7 +35,7 @@ def main():
 
     to_dump = p.to_json()
     with open(os.path.join(DATA_DIR, "parsed2.json"), "w") as f:
-        json.dump(to_dump, f, ident=4)
+        json.dump(to_dump, f, indent=4)
 
 
 if __name__ == "__main__":
